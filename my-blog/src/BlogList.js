fcd2ff422a3441569010483054f4ee4d7737ca6f
affiq23 <affiqem@gmail.com>
2023-12-28 22:34:57 -0600

    useEffect dependencies

--- my-blog/src/BlogList.js
+++ my-blog/src/BlogList.js
@@ -1,12 +1,16 @@
 const BlogList = (props) => {
-
     const blogs = props.blogsProps;
+    const title = props.title;
+    const handleDelete = props.handleDelete;
+
     return ( 
         <div className="blog-list">
+            <h1>{title}</h1>
             {blogs.map((blog) => (
             <div className="blog-preview" key = {blog.id}>
                 <h2>{blog.title}</h2>
                 <h4>Written by {blog.author}</h4>
+                <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                <br />
             </div>
         ))}
--- my-blog/src/Home.js
+++ my-blog/src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,10 +8,23 @@ const Home = () => {
         { title: "Welcome party!", body: "lorem ipsum...", author: "luigi", id: 2 },
         { title: "Webdev top tips!", body: "lorem ipsum...", author: "mario", id: 3 }
     ]);
+    const [name, setName] = useState("mario");
+
+    const handleDelete = (id) => {
+       const newBlogs = blogs.filter((blog) => blog.id !== id);
+       setBlogs(newBlogs);                                                                                                                       
+    }
+
+    useEffect(() => {
+        console.log("useEffect ran");
+        console.log(blogs);
+    }, [name]); // now useEffect only runs if name is changed, not if the whole page renders
     
     return (  
         <div className="home">
-            <BlogList blogsProps = {blogs} />
+            <BlogList blogsProps = {blogs} title = "All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName("luigi")}>Change name</button>
+            <p>{name}</p>
         </div>
     );
 }
